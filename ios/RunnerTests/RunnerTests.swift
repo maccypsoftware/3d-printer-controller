924e2a583893baedcedab15fe2a035b91415c553
Maciej Cypukow <maccypsoftware@gmail.com>
2026-04-17 09:26:15 +0200

    fix v4

---
+++ ios/RunnerTests/RunnerTests.swift
@@ -0,0 +1,14 @@
+import Flutter
+import UIKit
+import XCTest
+
+class RunnerTests: XCTestCase {
+  
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+    XCTAssertEqual(2 + 2, 4)
+  }
+  
+}
